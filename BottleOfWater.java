362125178f3fec900677c1ff6232396e624dec68
bellk <bellkiss-masher@yandex.ru>
2023-10-22 15:43:53 +0300

    bottle

---
+++ BottleOfWater.java
@@ -0,0 +1,17 @@
+import java.math.BigDecimal;
+
+/**
+ * Класс бутилированных напитков
+ */
+public class BottleOfWater extends Drinkables {
+    /**
+     * Перегруженный конструктор класса Бутилированных напитков
+     *
+     * @param name   Наименование напитка
+     * @param price  Стоимость напитка
+     * @param volume Объем напитка
+     */
+    public BottleOfWater(String name, BigDecimal price, Double volume) {
+        super(name, price, volume);
+    }
+}
